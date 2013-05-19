8f3950ef85faf46dc4586393edab858f544ce574
Dima Pasechnik <dima@ntu.edu.sg>
2013-05-19 19:26:11 +0800

    Settled the example2 issue by making the matrix symmetric. Removing .dat file as unneeded.

--- Examples/example2.py
+++ Examples/example2.py
@@ -1,39 +1,39 @@
 ######################################################################
-#	This program shows how to use SDP class to solve the 
-#	following semidefinite program, using CvxOpt and CSDP
-#	with numpy matrices:
+#   This program shows how to use SDP class to solve the 
+#   following semidefinite program, using CvxOpt and CSDP
+#   with numpy matrices:
 #
-#	maximize tr(C*X)
-#	Subject to:
-#			tr(A1*X) = 1
-#			tr(A2*X) =-1
-#			tr(A3*X) = 1
-#			X >= 0 (X is PSD)
+#   maximize tr(C*X)
+#   Subject to:
+#           tr(A1*X) = 1
+#           tr(A2*X) =-1
+#           tr(A3*X) = 1
+#           X >= 0 (X is PSD)
 #
-#	where
-#	C = [-33 9
-#	      9 -26
-#				-14 -9 -40
-#				-9  -91 -10
-#				-40 -10 -15]
+#   where
+#   C = [-33 9
+#         9 -26
+#               -14 -9 -40
+#               -9  -91 -10
+#               -40 -10 -15]
 #
-#	A1 = [7  11
-#	      11 -3
-#				21  11  0
-#				11 -10 -8
-#				 0 -8  -5]
+#   A1 = [7  11
+#         11 -3
+#               21  11  0
+#               11 -10 -8
+#                0 -8  -5]
 #
-#	A2 = [-7 18
-#	       18 -8
-#				 0  -10 -16
-#				-10  10  10
-#				-16  10 -3 ]
+#   A2 = [-7 18
+#          18 -8
+#                0  -10 -16
+#               -10  10  10
+#               -16  10 -3 ]
 #
-#	A3 = [2  8
-#	      8 -1
-#				 5  -2 17
-#				-2   6  7
-#				17  -8 -6]
+#   A3 = [2  8
+#         8 -1
+#                5  -2 17
+#               -2   6 -8 
+#               17  -8 -6]
 #
 ######################################################################
 
@@ -42,36 +42,36 @@ import numpy
 
 # Initializing C as 3 block matrices
 C = [
-	numpy.matrix([[-33,9],
-				  [9,-26]]), 
+    numpy.matrix([[-33,9],
+                  [9,-26]]), 
     numpy.matrix([[-14,-9,-40],
-				  [-9,-91,-10],
-				  [-40,-10,-15]])
+                  [-9,-91,-10],
+                  [-40,-10,-15]])
 ]
 
 # Initializing the first constraint
 A1 = [
-	numpy.matrix([[7, 11],
-				  [11, -3]]),
+    numpy.matrix([[7, 11],
+                  [11, -3]]),
     numpy.matrix([[21, 11, 0],
-				  [11, -10, -8],
-				  [0, -8, -5]])
+                  [11, -10, -8],
+                  [0, -8, -5]])
 ]
 
 # Initializing the second constraint
 A2 = [
-	numpy.matrix([[-7, 18],
-				  [18, -8]]),
+    numpy.matrix([[-7, 18],
+                  [18, -8]]),
     numpy.matrix([[0, -10, -16],
-				  [-10, 10, 10],
-				  [-16, 10, -3]])
+                  [-10, 10, 10],
+                  [-16, 10, -3]])
 ]
 A3 = [
-	numpy.matrix([[2, 8],
-				  [8, -1]]),
+    numpy.matrix([[2, 8],
+                  [8, -1]]),
     numpy.matrix([[5, -2, 17],
-				  [-2, 6, 7],
-				  [17, -8, -6]])
+                  [-2, 6, -8],
+                  [17, -8, -6]])
 ]
 
 # Packing constraints
@@ -81,7 +81,8 @@ A = [A1, A2, A3]
 b = [1, -1, 1]
 
 # Creating a SDP object for CvxOpt solver
-Prob1= SDP.sdp(solver='cvxopt')
+Prob1= SDP.sdp(solver='cvxopt', 
+        Settings={'feastol':1e-10, 'detail':True, 'Iterations':150})
 
 # Solving the problem
 Prob1.solve(C, b, A)
